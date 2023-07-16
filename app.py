56998423852c9ac9f24b74ce0fa1b6dd66f4691b
HyunSon <aa000098@naver.com>
2023-07-17 08:56:16 +0900

    add

--- app.py
+++ app.py
@@ -8,39 +8,29 @@ account_book = pd.read_csv(file_path)
 def view_records():
     return account_book
 
-def input_records(분류,날짜,항목,금액,카테고리,메모):
+def input_records(분류,날짜,사용처,금액,카테고리,메모):
     global account_book
-    new_record = pd.DataFrame([[분류, 날짜, 항목, 금액, 카테고리, 메모]], columns=['분류', '날짜', '항목', '금액', '카테고리', '메모'])
-    account_book = account_book.append(new_record, index=True)
-    account_book.to_csv(file_path, index=True)
+    new_record = pd.DataFrame([[분류, 날짜, 사용처, 금액, 카테고리, 메모]], columns=['분류', '날짜', '사용처', '금액', '카테고리', '메모'])
+    account_book = pd.concat([account_book, new_record])
+    account_book.to_csv(file_path, index=False)
 
 
 def interface():
-    with gr.Blocks("가계부") as app_interface:
+    with gr.Blocks() as app_interface:
         with gr.Tab("조회"):
-            view_interface = gr.Interface(rf = view_records, label = "조회", inputs=None, outputs="dataframe")
-            view_interface.launch()
-
+            view_interface = gr.Interface(fn = view_records, inputs=None, outputs="dataframe", title="가계부")
+        
         with gr.Tab("입력"):
-            input_text = gr.Interface(fn=input_records, inputs=[gr.Checkbox("수입"),
-                                                                gr.Checkbox("지출"), 
-                                                                "text", "text", "text", "number", "text", "text"],
-                                                                outputs=None, title="가계부 입력")
-            input_text.launch()
+            분류 = gr.Radio(["수입", "지출"], label="분류")
+            날짜 = gr.Textbox(label="날짜",placeholder="YYYY-MM-DD")
+            사용처 = gr.Textbox(label="사용처", placeholder="사용처")
+            금액 = gr.Number(label="금액")
+            카테고리 = gr.Dropdown(["식비", "카페ㆍ간식", "편의점ㆍ마트ㆍ잡화", "술ㆍ유흥", "쇼핑", "취미ㆍ여가", "의료ㆍ건강ㆍ피트니스", "주거ㆍ통신", "보험ㆍ세금ㆍ기타금융", "미용", "교통ㆍ자동차", "여행ㆍ숙박", "교육", "생활", "기부ㆍ후원", "카테고리 없음", "ATM 출금", "이체", "급여", "저축ㆍ투자"], label="카테고리")
+            메모 = gr.Textbox(label="메모", placeholder="메모를 남겨보세요")
+            input_button = gr.Button("입력하기")
+            input_button.click(input_records, inputs=([분류,날짜,사용처,금액,카테고리,메모]), outputs=None)
 
-        with gr.Tab("수정"):
-            input_text = gr.Interface(fn=input_records, inputs=[gr.Checkbox("수입"),
-                                                                gr.Checkbox("지출"), 
-                                                                "text", "text", "text", "number", "text", "text"],
-                                                                outputs=None, title="가계부 입력")
-            input_text.launch()
-
-        with gr.Tab("삭제"):
-            input_text = gr.Interface(fn=input_records, inputs=[gr.Checkbox("수입"),
-                                                                gr.Checkbox("지출"), 
-                                                                "text", "text", "text", "number", "text", "text"],
-                                                                outputs=None, title="가계부 입력")
-            input_text.launch()
+            
 
     app_interface.launch()
 
--- test.py
+++ test.py
@@ -1,25 +1,60 @@
-import pandas as pd
 import gradio as gr
+import pandas as pd
+import csv
 
-# AccountBook.csv 파일을 데이터프레임으로 불러오기
 file_path="AccountBook.csv"
 account_book = pd.read_csv(file_path)
 
-# 데이터프레임을 출력하는 함수
+
 def view_records():
     return account_book
 
-# Gradio 인터페이스 생성
+def input_records(분류,날짜,사용처,금액,카테고리,메모):
+    global account_book
+    new_record = pd.DataFrame([[분류, 날짜, 사용처, 금액, 카테고리, 메모]], columns=['분류', '날짜', '사용처', '금액', '카테고리', '메모'])
+    account_book = pd.concat([account_book, new_record])
+    account_book.to_csv(file_path, index=False)
+
+def correct_records(인덱스,분류,날짜,사용처,금액,카테고리,메모):
+    global account_book
+    account_book.iloc[인덱스,] = [분류,날짜,사용처,금액,카테고리,메모]
+    account_book.to_csv(file_path, index=False)
+
+def view_index():
+    account_book = pd.read_csv(file_path)
+    account_book.index += 1
+    df_with_index = account_book.reset_index()
+    df_with_index.index += 1
+    df_with_index.columns = ['인덱스'] + list(account_book.columns)
+    return df_with_index
+
 def interface():
     with gr.Blocks() as app_interface:
         with gr.Tab("조회"):
-            iface = gr.Interface(fn=view_records, 
-                     title='Account Book Viewer', 
-                     description='View Account Book DataFrame',
-                     layout='wide',
-                     inputs=None,  # 입력 없음
-                     outputs="text")  # 출력 형식은 텍스트로 설정)
-            iface.launch()
+            view_interface = gr.Interface(fn = view_records, inputs=None, outputs="dataframe", title="가계부")
+
+        with gr.Tab("입력"):
+            분류 = gr.Radio(["수입", "지출"], label="분류")
+            날짜 = gr.Textbox(label="날짜",placeholder="YYYY-MM-DD")
+            사용처 = gr.Textbox(label="사용처", placeholder="사용처")
+            금액 = gr.Number(label="금액")
+            카테고리 = gr.Dropdown(["식비", "카페ㆍ간식", "편의점ㆍ마트ㆍ잡화", "술ㆍ유흥", "쇼핑", "취미ㆍ여가", "의료ㆍ건강ㆍ피트니스", "주거ㆍ통신", "보험ㆍ세금ㆍ기타금융", "미용", "교통ㆍ자동차", "여행ㆍ숙박", "교육", "생활", "기부ㆍ후원", "카테고리 없음", "ATM 출금", "이체", "급여", "저축ㆍ투자"], label="카테고리")
+            메모 = gr.Textbox(label="메모", placeholder="메모를 남겨보세요")
+            input_button = gr.Button("입력하기")
+            input_button.click(input_records, inputs=([분류,날짜,사용처,금액,카테고리,메모]), outputs=None)
+
+        with gr.Tab("수정"):
+            인덱스 = gr.Number(label="수정하고 싶은 데이터의 인덱스")
+            분류 = gr.Radio(["수입", "지출"], label="분류")
+            날짜 = gr.Textbox(label="날짜",placeholder="YYYY-MM-DD")
+            사용처 = gr.Textbox(label="사용처", placeholder="사용처")
+            금액 = gr.Number(label="금액")
+            카테고리 = gr.Dropdown(["식비", "카페ㆍ간식", "편의점ㆍ마트ㆍ잡화", "술ㆍ유흥", "쇼핑", "취미ㆍ여가", "의료ㆍ건강ㆍ피트니스", "주거ㆍ통신", "보험ㆍ세금ㆍ기타금융", "미용", "교통ㆍ자동차", "여행ㆍ숙박", "교육", "생활", "기부ㆍ후원", "카테고리 없음", "ATM 출금", "이체", "급여", "저축ㆍ투자"], label="카테고리")
+            메모 = gr.Textbox(label="메모", placeholder="메모를 남겨보세요")
+            input_button = gr.Button("수정하기")
+            input_button.click(correct_records, inputs=([인덱스,분류,날짜,사용처,금액,카테고리,메모]), outputs=None)
+            
+            index_interface = gr.Interface(fn = view_index, inputs=None, outputs="dataframe", title="가계부")
 
     app_interface.launch()
 
