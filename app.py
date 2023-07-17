29bdb60b75c246178b702f41e433361a94066169
HyunSon <aa000098@naver.com>
2023-07-17 13:27:02 +0900

    del

--- app.py
+++ app.py
@@ -6,32 +6,73 @@ file_path="AccountBook.csv"
 account_book = pd.read_csv(file_path)
 
 def view_records():
+    account_book = pd.read_csv(file_path)
     return account_book
 
 def input_records(분류,날짜,사용처,금액,카테고리,메모):
-    global account_book
+    account_book = pd.read_csv(file_path)
     new_record = pd.DataFrame([[분류, 날짜, 사용처, 금액, 카테고리, 메모]], columns=['분류', '날짜', '사용처', '금액', '카테고리', '메모'])
     account_book = pd.concat([account_book, new_record])
     account_book.to_csv(file_path, index=False)
 
+def correct_records(인덱스,분류,날짜,사용처,금액,카테고리,메모):
+    account_book = pd.read_csv(file_path)
+    account_book.loc[인덱스-1] = [분류,날짜,사용처,금액,카테고리,메모]
+    account_book.to_csv(file_path, index=False)
+
+def view_index():
+    account_book = pd.read_csv(file_path)
+    df_with_index = account_book.reset_index()
+    df_with_index.columns = ['인덱스'] + list(account_book.columns)
+    df_with_index['인덱스'] += 1
+    return df_with_index
+
+def delete_records(인덱스):
+    account_book = pd.read_csv(file_path)
+    val = account_book.loc[인덱스-1].to_frame().T
+    account_book.drop(인덱스-1, axis=0, inplace=True)
+    account_book.to_csv(file_path, index=False)
+    return val
+
+def 조회():
+    view_interface = gr.Interface(fn = view_records, inputs=None, outputs="dataframe", title="가계부", allow_flagging='never')
+
+def 입력():
+    분류 = gr.Radio(["수입", "지출"], label="분류")
+    날짜 = gr.Textbox(label="날짜",placeholder="YYYY-MM-DD")
+    사용처 = gr.Textbox(label="사용처", placeholder="사용처")
+    금액 = gr.Number(label="금액")
+    카테고리 = gr.Dropdown(["식비", "카페ㆍ간식", "편의점ㆍ마트ㆍ잡화", "술ㆍ유흥", "쇼핑", "취미ㆍ여가", "의료ㆍ건강ㆍ피트니스", "주거ㆍ통신", "보험ㆍ세금ㆍ기타금융", "미용", "교통ㆍ자동차", "여행ㆍ숙박", "교육", "생활", "기부ㆍ후원", "카테고리 없음", "ATM 출금", "이체", "급여", "저축ㆍ투자"], label="카테고리")
+    메모 = gr.Textbox(label="메모", placeholder="메모를 남겨보세요")
+    input_button = gr.Button("입력하기")
+    input_button.click(input_records, inputs=([분류,날짜,사용처,금액,카테고리,메모]), outputs=None)
+    
+def 수정():
+    인덱스 = gr.Number(label="수정하고 싶은 데이터의 인덱스")
+    분류 = gr.Radio(["수입", "지출"], label="분류")
+    날짜 = gr.Textbox(label="날짜",placeholder="YYYY-MM-DD")
+    사용처 = gr.Textbox(label="사용처", placeholder="사용처")
+    금액 = gr.Number(label="금액")
+    카테고리 = gr.Dropdown(["식비", "카페ㆍ간식", "편의점ㆍ마트ㆍ잡화", "술ㆍ유흥", "쇼핑", "취미ㆍ여가", "의료ㆍ건강ㆍ피트니스", "주거ㆍ통신", "보험ㆍ세금ㆍ기타금융", "미용", "교통ㆍ자동차", "여행ㆍ숙박", "교육", "생활", "기부ㆍ후원", "카테고리 없음", "ATM 출금", "이체", "급여", "저축ㆍ투자"], label="카테고리")
+    메모 = gr.Textbox(label="메모", placeholder="메모를 남겨보세요")
+    correction_button = gr.Button("수정하기")
+    correction_button.click(correct_records, inputs=([인덱스,분류,날짜,사용처,금액,카테고리,메모]), outputs=None)
+    index_interface = gr.Interface(fn = view_index, inputs=None, outputs="dataframe", title="가계부", allow_flagging='never')
+
+def 삭제():
+    gr.Interface(fn=delete_records, inputs="number", outputs="dataframe", allow_flagging='never')
 
 def interface():
     with gr.Blocks() as app_interface:
         with gr.Tab("조회"):
-            view_interface = gr.Interface(fn = view_records, inputs=None, outputs="dataframe", title="가계부")
-        
+            조회()
         with gr.Tab("입력"):
-            분류 = gr.Radio(["수입", "지출"], label="분류")
-            날짜 = gr.Textbox(label="날짜",placeholder="YYYY-MM-DD")
-            사용처 = gr.Textbox(label="사용처", placeholder="사용처")
-            금액 = gr.Number(label="금액")
-            카테고리 = gr.Dropdown(["식비", "카페ㆍ간식", "편의점ㆍ마트ㆍ잡화", "술ㆍ유흥", "쇼핑", "취미ㆍ여가", "의료ㆍ건강ㆍ피트니스", "주거ㆍ통신", "보험ㆍ세금ㆍ기타금융", "미용", "교통ㆍ자동차", "여행ㆍ숙박", "교육", "생활", "기부ㆍ후원", "카테고리 없음", "ATM 출금", "이체", "급여", "저축ㆍ투자"], label="카테고리")
-            메모 = gr.Textbox(label="메모", placeholder="메모를 남겨보세요")
-            input_button = gr.Button("입력하기")
-            input_button.click(input_records, inputs=([분류,날짜,사용처,금액,카테고리,메모]), outputs=None)
-
-            
-
+            입력()
+        with gr.Tab("수정"):
+            수정()
+        with gr.Tab("삭제"):
+            삭제()
+        
     app_interface.launch()
 
 if __name__ == "__main__":
--- test2.py
+++ test2.py
@@ -1,30 +1,8 @@
 import gradio as gr
 import pandas as pd
 
-# 임의의 데이터프레임 생성
-data = {
-    'Name': ['John', 'Alice', 'Bob'],
-    'Age': [25, 30, 35],
-    'City': ['New York', 'Paris', 'London']
-}
-df = pd.DataFrame(data)
 
-def modify_dataframe(name, age, city):
-    # 데이터프레임 수정
-    df.loc[df['Name'] == name, 'Age'] = age
-    df.loc[df['Name'] == name, 'City'] = city
-    return df
+file_path="AccountBook.csv"
+account_book = pd.read_csv(file_path)
 
-# Gradio 인터페이스 설정
-name_input = gr.inputs.Dropdown(choices=df['Name'].tolist(), label="Name")
-age_input = gr.inputs.Number(label="Age")
-city_input = gr.inputs.Textbox(label="City")
-output_text = gr.outputs.Textbox(label="Modified DataFrame")
-
-def update_dataframe(name, age, city):
-    modified_df = modify_dataframe(name, age, city)
-    return modified_df.to_string(index=False)
-
-# Gradio 인터페이스 실행
-iface = gr.Interface(fn=update_dataframe, inputs=[name_input, age_input, city_input], outputs=output_text)
-iface.launch()
+print(account_book.loc[1].to_frame().T)
